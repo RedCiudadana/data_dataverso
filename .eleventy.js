37a2b6e01e617516281cf7b6a3b67866835e6bc1
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-02-02 11:35:54 -0600

    new try

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const moment = require("moment");
 const now = new Date();
-const rmj = require('render-markdown-js')
+const rmj = require('render-markdown-js');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = function (eleventyConfig) {
 
@@ -9,6 +11,19 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src');
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('admin');
+
+    eleventyConfig.addFilter('read', function (relativePath) {
+        // Construct the absolute path to the file
+        const absolutePath = path.join(__dirname, relativePath);
+    
+        // Use the 'fs' module to read the file synchronously
+        return fs.readFileSync(absolutePath, 'utf-8');
+    });
+    
+    // Add a filter for jsonify
+    eleventyConfig.addFilter('jsonify', function (value) {
+    return JSON.stringify(value, null, 2);
+    });
 
     eleventyConfig.addNunjucksFilter("rmj", function(content) {
         return rmj(content);
